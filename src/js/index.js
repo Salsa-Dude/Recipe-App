3fe7a02142697cf1b10de5fa12b68765f9e2d5f5
Joseph Arias <arias.joseph.a@gmail.com>
2018-06-13 19:24:45 -0400

    Search Module

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,9 @@
 
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = '48d3fe39a857ced9e3769ab9799d9d94';
-  try {
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch(err) {
-    alert(err);
-  }
-  
-}
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
 
-getResults('chicken');
 
 
---
+++ src/js/models/Search.js
@@ -0,0 +1,20 @@
+
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '48d3fe39a857ced9e3769ab9799d9d94';
+    try {
+      const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch(err) {
+      alert(err);
+    }
+  }
+}
