56b98c9949c8ce2ecf559f7e1e1ebdcc08984617
Joseph Arias <arias.joseph.a@gmail.com>
2018-06-13 18:43:07 -0400

    Setting API

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
-// Global app controller
 
-import x from './test.js';
+import axios from 'axios';
 
-console.log(`I imported ${x} from another module updated`);
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = '48d3fe39a857ced9e3769ab9799d9d94';
+  try {
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch(err) {
+    alert(err);
+  }
+  
+}
+
+getResults('chicken');
+
+
--- src/js/test.js
+++
@@ -1,4 +0,0 @@
-
-console.log('Imported Module');
-
-export default 1234910;
